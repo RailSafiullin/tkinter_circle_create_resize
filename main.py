d8b9e3f9e0300cc02904f9c60b368ed062b97bc8
RailSafiullin <rail.safiyllin@icloud.com>
2023-11-25 04:28:30 +0400

    add create circle window

--- main.py
+++ main.py
@@ -32,6 +32,15 @@ class CircleApp:
         self.root.bind_all("<Escape>", self.exit_program)
         self.root.bind_all("<Return>", self.focus_root)
 
+        create_circle_button = tk.Button(
+            self.root, text="Пересоздать круг", command=self.create_circle
+        )
+        self.input_dialog = None
+        create_circle_button.pack()
+
+    def run(self):
+        self.root.mainloop()
+
     def increase_radius(self, event):
         self.root.focus()
         step = int(self.ent1.get())
@@ -63,15 +72,52 @@ class CircleApp:
         self.canvas.itemconfig(
             self.radius_display, text=f"Радиус: {self.circle_radius}"
         )
+
+    def create_circle(self):
+        self.input_dialog = tk.Toplevel()
+        self.input_dialog.title("Создать круг")
+
+        tk.Label(self.input_dialog, text="X-координата центра:").grid(row=0, column=0)
+        tk.Label(self.input_dialog, text="Y-координата центра:").grid(row=1, column=0)
+        tk.Label(self.input_dialog, text="Радиус:").grid(row=2, column=0)
+
+        self.x_entry = tk.Entry(self.input_dialog)
+        self.y_entry = tk.Entry(self.input_dialog)
+        self.radius_entry = tk.Entry(self.input_dialog)
+
+        self.x_entry.insert(0, 450)
+        self.y_entry.insert(0, 150)
+        self.radius_entry.insert(0, 10)
+
+        self.x_entry.grid(row=0, column=1)
+        self.y_entry.grid(row=1, column=1)
+        self.radius_entry.grid(row=2, column=1)
+
+        create_button = tk.Button(
+            self.input_dialog, text="Создать", command=self.create_circle_action
+        )
+        create_button.grid(row=3, columnspan=2)
+
+    def create_circle_action(self):
+        self.canvas.delete(self.circle)
+        self.x = int(self.x_entry.get())
+        self.y = int(self.y_entry.get())
+        self.circle_radius = int(self.radius_entry.get())
+
+        self.circle = self.canvas.create_oval(
+            self.x - self.circle_radius,
+            self.y - self.circle_radius,
+            self.x + self.circle_radius,
+            self.y + self.circle_radius,
+            width=1,
+        )
+        self.input_dialog.destroy()
+
     def exit_program(self, event):
         self.root.destroy()
 
     def focus_root(self, event):
         self.root.focus()
-    
-    def run(self):
-        self.root.mainloop()
-
 
 
 if __name__ == "__main__":
